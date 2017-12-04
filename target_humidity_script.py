1ae3f43fa33921252fc8855ae2c9d28e648cbdb2
DaveL17 <dave.lehrer@icloud.com>
2017-12-04 16:23:33 -0600

    Simplifies the code a bit to match the wiki instructions.

--- target_humidity_script.py
+++ target_humidity_script.py
@@ -1,43 +1,48 @@
-#!/usr/bin/env python2.5
+#!/usr/bin/env python2.6
 # -*- coding: utf-8 -*-
 
 """
 Target Humidities based on best-practice targets.  Source: Trane
 ==========================
-Outdoor         Indoor
+Outdoor       Indoor
 Temp         Humidity
 +40            45%
 +30            40%
 +20            35%
 +10            30%
-0               25%
+  0            25%
 -10            20%
 -20            15%
 """
 
-# Put the humidity level into a variable to drive the triggers. (You can't currently compare a device value to a variable value directly within a trigger.)
-currentHumidity = indigo.devices[281604201].states["sensorValue"]
-currentHumidity = int(currentHumidity)
-currentHumidity = str(currentHumidity)
-indigo.variable.updateValue(950128135, currentHumidity)
+current_humidity = indigo.devices[281604201].states["sensorValue"]
+current_temperature = float(indigo.devices[1899035475].states["temp"])
+current_humidity_var = 950128135
+target_numidity_var_id = 187913970
 
-# Get the current outdoor temperature.
-currentTemp = float(indigo.devices[1899035475].states["temp"])
+# Put the humidity level into a variable to drive the triggers.
+# =============================================================
+# (You can't currently compare a device state value to a variable value
+# directly within a trigger.)
+
+current_humidity = int(current_humidity)
+current_humidity = str(current_humidity)
+indigo.variable.updateValue(current_humidity_var, current_humidity)
 
 # not more than 45% humidity
-if currentTemp >= 40:
+if current_temperature >= 40:
     indigo.server.log(u'Updating target humidity level to: 45.')
-    indigo.variable.updateValue(187913970, '45')
+    indigo.variable.updateValue(target_numidity_var_id, '45')
 
 # not less than 15% humidity
-elif currentTemp < -20:
+elif current_temperature < -20:
     indigo.server.log(u'Updating target humidity level to: 15.')
-    indigo.variable.updateValue(187913970, '15')
+    indigo.variable.updateValue(target_numidity_var_id, '15')
 
 # temperature is between -20 and 40
 else:
-    targetHumidity = 45 - ((40 - currentTemp) / 2)
-    targetHumidity = int(targetHumidity)
-    targetHumidity = str(targetHumidity)
-    indigo.server.log(u'Updating target humidity level to: %s.' % targetHumidity)
-    indigo.variable.updateValue(187913970, targetHumidity)
+    target_humidity = 45 - ((40 - current_temperature) / 2)
+    target_humidity = int(target_humidity)
+    target_humidity = str(target_humidity)
+    indigo.server.log(u'Updating target humidity level to: {0}.'.format(target_humidity))
+    indigo.variable.updateValue(target_numidity_var_id, target_humidity)
